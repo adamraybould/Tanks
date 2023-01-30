52c13c8050142d34cc7647272858f49c1ca3bd0b
Adam <adam.raybould22@gmail.com>
2023-01-30 20:05:15 +0000

    - Initial Commit

---
+++ Source/Tanks/TanksGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TanksGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATanksGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATanksGameMode();
+};
+
+
+
